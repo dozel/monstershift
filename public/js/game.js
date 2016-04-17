e163a034d90c347a9fb3e9475e11de96a120e844
Sean Dunford <sdunford@mail.usf.edu>
2016-04-17 13:54:03 -0700

    Push

--- public/js/game.js
+++ public/js/game.js
@@ -9,7 +9,7 @@ function preload() {
     gameWorld = game.add.group();
 }
 
-var ZOOM_OUT = true;
+var ZOOM_OUT = false;
 
 function create() {
     game.world.setBounds(0,0,4000,2400);
--- public/js/setup.js
+++ public/js/setup.js
@@ -56,4 +56,3 @@ $.extend(Setup.prototype, {
 
     }
 });
-
